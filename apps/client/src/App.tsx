7c2778a2794d5fce4d0d51581fcf8e73ef14e2ec
kimitashoichi <kimi.syou.koma@gmail.com>
2024-01-27 17:15:51 +0900

    ヘッダーメニューの作成

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { HeaderComponent } from './components/util/Header';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <HeaderComponent />
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ apps/client/src/components/util/Header.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Layout } from 'antd';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+import { CodeOutlined, DesktopOutlined, ReadOutlined, AlertOutlined, MailOutlined } from '@ant-design/icons';
+
+
+const { Header } = Layout;
+const items: MenuProps['items'] = [
+  {
+    label: 'Career',
+    key: 'career',
+    icon: <AlertOutlined />,
+  },
+  {
+    label: 'Skills',
+    key: 'skills',
+    icon: <CodeOutlined />,
+  },
+  {
+    label: 'Achievements',
+    key: 'achievements',
+    icon: <DesktopOutlined />,
+  },
+  {
+    label: 'Blogs',
+    key: 'blogs',
+    icon: <ReadOutlined />,
+  },
+  {
+    label: 'Contact',
+    key: 'contact',
+    icon: <MailOutlined />,
+  },
+];
+
+
+
+export const HeaderComponent = () => {
+  return (
+    <Header>
+      <div style={{
+          display: 'flex',
+          justifyContent: 'space-between'
+        }}>
+        <div>
+          Shoichi Kimita
+        </div>
+        <Menu mode="horizontal" items={items} />
+      </div>
+    </Header>
+  )
+}
