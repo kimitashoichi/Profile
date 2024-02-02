c41eed7ae4a1e91928bc90f62038d5669bc905e9
kimitashoichi <kimi.syou.koma@gmail.com>
2024-02-02 20:46:01 +0900

    お問い合わせフォームのファイルだけ作成

---
+++ apps/client/src/components/contact-section/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RHF from 'react-hook-form';
+
+// TODO: React Hook　Formでフォーム作成
+// sendgridを利用して自分のGメールに転送するAPIを作成
+
+export const ContactComponent = () => {
+  return (
+    <>
+      <RHF.Form>
+        
+      </RHF.Form>
+    </>
+  )
+}
